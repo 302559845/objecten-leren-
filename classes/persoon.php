7eb583be461b1509702a2447fbc1042756367fa7
unknown <302559845@student.rocmondriaan.nl>
2022-09-01 14:44:43 +0200

    bezig

---
+++ classes/persoon.php
@@ -0,0 +1,14 @@
+<?php
+
+class persoon
+{
+public $naam;
+private $leeftijd;
+protected $geslacht;
+public $isStudent;
+public $gemiddeldCijfer;
+function __construct(string $naam, int $leeftijd, string $geslacht )
+    
+{
+}
+}
