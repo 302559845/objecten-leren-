2f3c13f0ec9af4ea4b1d4748ca1fcfe9dee55e7d
unknown <302559845@student.rocmondriaan.nl>
2022-09-01 15:10:22 +0200

    make changes

--- classes/persoon.php
+++ classes/persoon.php
@@ -7,8 +7,11 @@ private $leeftijd;
 protected $geslacht;
 public $isStudent;
 public $gemiddeldCijfer;
-function __construct(string $naam, int $leeftijd, string $geslacht )
-    
-{
+function __construct(string $naam, int $leeftijd, string $geslacht ){
+    $this->naam = $naam;
+    $this->leeftijd = $leeftijd;
+    $this->geslacht = $geslacht;
+    echo"\n Nieuw persoon-object aangemaakt" . "<br>";
+    echo"\n De property naam is " . $this->naam . "<br>";
 }
 }
